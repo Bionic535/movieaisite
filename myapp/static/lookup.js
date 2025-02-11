510c5495c9d98bc0d1e3fca7b988379f2b90d7cf
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-02-11 11:30:44 +0800

    first commit

---
+++ myapp/static/lookup.js
@@ -0,0 +1,37 @@
+let file = './static/movie_dataset.csv'
+
+console.clear();
+const form = document.getElementById("movies");
+fetch(file)
+    .then(Response => Response.text())
+    .then(csvText => {
+        const rows = csvText.split('\n');
+        console.log(rows)
+        const names = rows
+            .map(trimarr)
+            .filter((title) => title);
+        addtolist(names)
+    })
+
+
+function addtolist(headers) {
+    for (let i = 1; i < headers.length; i++) {
+        opt = document.createElement("option");
+        opt.value = headers[i];
+        opt.textContent = headers[i];
+        form.appendChild(opt);
+        
+    }
+}
+
+function trimarr(filmdata) {
+    const film = filmdata.split(",");
+    if (film.length > 1) {
+        return film[7].trim();
+    }
+    return null
+}
+
+
+
+
---
+++ myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+import model
+# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def films(request):
+    if request.method == 'POST':
+        name = request.POST.get('movie')
+        numb = request.POST.get('numb')
+        context = []
+        try:
+            name = str(name)
+            numb = int(numb)
+            context = model.model(name, numb)
+        except ValueError:
+            name = None
+            numb = None
+        
+        return render(request, 'films.html', {'films': context})
+    else:
+        return render(request, 'home.html')
+        
