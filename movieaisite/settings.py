51584acaaeb5f39e89c637b431bcb4cb6bf404c8
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-03-13 21:12:39 +0800

    push to railway

--- movieaisite/settings.py
+++ movieaisite/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'movieaisite-production.up.railway.app' ]
 
-
+CSRF_TRUSTED_ORIGINS = [ 'https://movieaisite-production.up.railway.app' ]
 # Application definition
 
 INSTALLED_APPS = [
