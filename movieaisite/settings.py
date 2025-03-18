56f7fac420e0a6364ae22acb212ce6c62490185d
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-03-18 22:02:39 +0800

    railway 6

--- movieaisite/settings.py
+++ movieaisite/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'movieaisite.up.railway.app' ]
+ALLOWED_HOSTS = [ 
+    "movieaisite.up.railway.app",
+    "0.0.0.0",
+    "localhost"
+]
 CSRF_TRUSTED_ORIGINS = [ 'https://movieaisite.up.railway.app' ]
 
 # Logging configuration
