cf088266e510b30c92e5a91a18717a8abe98f853
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-07-02 07:48:29 +0800

    reset register progress

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("films", views.films, name = "films"),
     path('api/movie_titles/', views.movie_titles, name='movie_titles'),
-    path("register/", views.RegisterView, name="register"),
-    path("login/", views.LoginView, name="login"),
 ]
