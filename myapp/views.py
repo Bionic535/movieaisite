576032d408b5074892d9833991ef6d9b2a974360
Lachlan Bruce <lachlan.bruce@outlook.com>
2025-07-01 21:41:50 +0800

    register done backend

--- myapp/views.py
+++ myapp/views.py
@@ -42,7 +42,34 @@ def films(request):
 
 
 def RegisterView(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password1')
+        confirm_password = request.POST.get('password2')
+
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match.")
+            return redirect('register')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return redirect('register')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists.")
+            return redirect('register')
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password
+        )
+        messages.success(request, "Registration successful. You can now log in.")
+        return redirect('login')
+    else:
+         return render(request, 'register.html')
 
 def LoginView(request):
     return render(request, 'login.html')
